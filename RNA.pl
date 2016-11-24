e8a60f5a5e282850e051b505e674da207ec0efc3
dareorture <1114748177@qq.com>
2016-11-24 10:36:08 +0800

    RNA

--- RNA.pl
+++ RNA.pl
@@ -3,7 +3,7 @@ $DNA = "ACGGGTACTTGGCCTTTA";
 print "Here is the staring DNA:\n\n";
 print $DNA,"\n\n";
 $RNA = $DNA;
-$RNA = ~ s/T/U/g;
+$RNA =~ s/T/U/g;
 print "Here is the result of transcribing the DNA to RNA:\n\n";
 print $RNA,"\n\n";
 
